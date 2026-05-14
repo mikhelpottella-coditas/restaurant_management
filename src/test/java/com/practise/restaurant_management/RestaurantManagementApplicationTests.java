683b89f72968e9a46c4539e0194070389d69b498
mikhelpottella-coditas <mikhel.pottella@coditas.com>
2026-05-14 17:34:59 +0530

    first commit

---
+++ src/test/java/com/practise/restaurant_management/RestaurantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.restaurant_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
